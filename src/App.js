7b9e40991b790cb92379384b3014792f09548028
Loganwemploy <loganwemploy@gmail.com>
2024-07-18 17:57:56 -0500

    add basic hero stylesand links

--- src/App.js
+++ src/App.js
@@ -139,7 +139,7 @@ return (
       </div>
    {/*  */}
       <div class="md:w-2/5 xl:w-[45%]">
-    <h2 class="text-4xl font-bold text-gray-800 mt-10 md:mt-0" id="imgtext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium deleniti consequatur illum sit recusandae fugit sunt possimus iure laborum facilis.</h2>
+    <h2 class="text-4xl font-bold text-gray-800 mt-10 md:mt-0" id="imgtext">Harmonize Body and Mind with Biodynamic Craniosacral Therapy</h2>
     <p class="mt-3.5 mb-6 flex gap-1 items-center text-lg">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="text-gray-400 w-5 h-5">
         <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
